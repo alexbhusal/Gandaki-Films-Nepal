f83ef8075ff8bf0c9c2f76bea6c4d21771fbeea6
Alex <alexbhusal@icloud.com>
2025-03-28 15:22:02 +0545

    package updated

---
+++ constants/priceData.js
@@ -0,0 +1,167 @@
+const WeedingPrice = [
+  {
+    id: 1,
+    bride: {
+      title: "Best Bride Package",
+      description: "The best option for the bride",
+      price: "35K",
+      includes: [
+        "Photo & Video  (1Day)",
+        "Edited Photo & Video",
+        "Pendrive",
+        "Couple Photo Framing",
+        "Family Photo Framing",
+        "Extra Gift By Company...."
+      ],
+      excluded: [
+         "200 Photos Album",
+        "Karizma Album (22 Sheet)"
+      ],
+      buttonText: "Bride Package 1"
+    },
+    groom: {
+      title: "Best Groom Package",
+      description: "The best option for the groom ",
+      price: "55K",
+      includes: [
+        "Photo & Video (2 Day)",
+        "Edited Photo & Video",
+        "Post-Wedding",
+        "Pendrive",
+        "Normal Male Framing",
+        "Family Photo Framing",
+        "100 Photos Album", 
+        "Extra Gift By Company...."
+      ],
+      excluded: [
+        "Karizma Album (22 Sheet)",
+        "Drone Video",
+      ],
+      buttonText: "Groom Package 1"
+    }
+  },
+
+  {
+    id: 2,
+    bride: {
+      title: "Value for Money Bride",
+      description: "Affordable yet stylish",
+      price: "45K",
+      includes: [
+        "Photo & Video  (1Day)",
+        "Edited Photo & Video",
+        "Pendrive",
+        "Couple Photo Framing",
+        "Family Photo Framing",
+        "Extra Gift By Company....",
+        "200 Photos Album"
+      ],
+      excluded: [
+        "Karizma Album (22 Sheet)"
+      ],
+      buttonText: "Bride Package 2"
+    },
+    groom: {
+      title: "Value for Money Groom",
+      description: "Affordable yet stylish",
+      price: "75K",
+      includes: [
+        "Photo & Video (2Day)",
+        "Edited Photo & Video",
+        "Post-Wedding",
+        "Pendrive",
+        "Heavy Male Framing",
+        "Weeding Dress Framing",
+        "Family Photo Framing",
+        "220 Photos Album", 
+        "Extra Gift By Company....",
+      ],
+      excluded: [
+        "Karizma Album (22 Sheet)",
+        "Drone Video",
+      ],
+      buttonText: "Groom Package 2"
+    }
+  },
+  {
+    id: 3,
+    bride: {
+      title: "Enterprise Bride",
+      description: "For the bride who wants the full ",
+      price: "55K",
+      includes: [
+       "Photo & Video  (1Day)",
+        "Edited Photo & Video",
+        "Pendrive",
+        "Couple Photo Framing",
+        "Family Photo Framing",
+        "Extra Gift By Company....",
+        "200 Photos Album",
+        "Karizma Album (22 Sheet)"
+      ],
+      excluded: [
+       
+      ],
+      buttonText: "Bride Package 3"
+    },
+    groom: {
+      title: "Enterprise Groom",
+      description: " groom who wants a premium .",
+      price: "95K",
+      includes: [
+        "Photo & Video (2Day)",
+        "Edited Photo & Video",
+        "Post-Wedding",
+        "Pendrive",
+        "Heavy Male Framing",
+        "Weeding Dress Framing",
+        "Family Photo Framing",
+        "220 Photos Album", 
+        "Extra Gift By Company....",
+        "Karizma Album (22 Sheet)",
+      ],
+      excluded: [
+        "Drone Video",
+      ],
+      buttonText: "Groom Package 3"
+    }
+  },
+  {
+    id: 4,
+    bride: {
+      title: "Negotiable Bride",
+      description: "As Per Required",
+      price: ".....",
+      includes: [
+        "Negotiable"
+      ],
+      excluded: [
+       
+      ],
+      buttonText: "Customize Bride Package"
+    },
+    groom: {
+      title: "Negotiable Groom",
+      description: "Custom packages ",
+      price: "115K",
+      includes: [
+        "Photo & Video (2Day)",
+        "Edited Photo & Video",
+        "Post-Wedding",
+        "Pendrive",
+        "Heavy Male Framing",
+        "Weeding Dress Framing",
+        "Family Photo Framing",
+        "220 Photos Album", 
+        "Extra Gift By Company....",
+        "Karizma Album (22 Sheet)",
+        "Drone Video",
+      ],
+      excluded: [
+      ],
+      buttonText: "Groom Package 4"
+    }
+  }
+];
+
+export default WeedingPrice;
